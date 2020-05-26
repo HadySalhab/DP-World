4522e3b7a01ec10132dc392523f1f46171a5e7cf
Hady Salhab <hadysalhab1@gmail.com>
2020-05-26 19:16:07 +1000

    Add Schemas

---
+++ app/src/main/java/com/android/myapplication/dp_world/data/DesignPatternSchema.java
@@ -0,0 +1,38 @@
+package com.android.myapplication.dp_world.data;
+
+import com.google.gson.annotations.SerializedName;
+
+public class DesignPatternSchema {
+
+    @SerializedName("id")
+    private final int mId;
+    @SerializedName("title")
+    private final String mTitle;
+    @SerializedName("description")
+    private final String mDescription;
+    @SerializedName("category")
+    private final String mCategory;
+
+    public DesignPatternSchema(int id, String title, String description,String category) {
+        mId = id;
+        mTitle = title;
+        mDescription = description;
+        mCategory = category;
+    }
+
+    public int getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public String getCategory() {
+        return mCategory;
+    }
+}
---
+++ app/src/main/java/com/android/myapplication/dp_world/data/DesignPatternsResponseSchema.java
@@ -0,0 +1,20 @@
+package com.android.myapplication.dp_world.data;
+
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class DesignPatternsResponseSchema {
+
+    @SerializedName("design_patterns")
+    private final List<DesignPatternSchema> mDesignPatterns;
+
+    public DesignPatternsResponseSchema(List<DesignPatternSchema> designPatternSchemas) {
+        mDesignPatterns = designPatternSchemas;
+    }
+
+    public List<DesignPatternSchema> getDesignPatterns() {
+        return mDesignPatterns;
+    }
+}
