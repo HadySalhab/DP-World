b32daccacecf6d12db5cdd66bf4bfb3c693b1716
Hady Salhab <hadysalhab1@gmail.com>
2020-05-27 18:33:54 +1000

    Extract ObservableViewMvc interface

--- app/src/main/java/com/android/myapplication/dp_world/screen/designpatternlist/DesignPatternListItemViewMvc.java
+++ app/src/main/java/com/android/myapplication/dp_world/screen/designpatternlist/DesignPatternListItemViewMvc.java
@@ -3,13 +3,12 @@ package com.android.myapplication.dp_world.screen.designpatternlist;
 import android.view.View;
 
 import com.android.myapplication.dp_world.dp.DesignPattern;
+import com.android.myapplication.dp_world.screen.common.ObservableViewMvc;
 import com.android.myapplication.dp_world.screen.common.ViewMvc;
 
-abstract class DesignPatternListItemViewMvc implements ViewMvc {
+abstract class DesignPatternListItemViewMvc implements ObservableViewMvc <DesignPatternListItemViewMvc.Listener> {
     public interface Listener {
         void onDesignPatternClicked(DesignPattern designPattern);
     }
-    abstract void registerListener(Listener listener);
-    abstract void unregisterListener(Listener listener);
     abstract void bindDesignPattern(DesignPattern designPattern);
 }
--- app/src/main/java/com/android/myapplication/dp_world/screen/designpatternlist/DesignPatternListViewMvc.java
+++ app/src/main/java/com/android/myapplication/dp_world/screen/designpatternlist/DesignPatternListViewMvc.java
@@ -1,17 +1,12 @@
 package com.android.myapplication.dp_world.screen.designpatternlist;
 
-import android.view.View;
-
 import com.android.myapplication.dp_world.dp.DesignPattern;
-import com.android.myapplication.dp_world.screen.common.ViewMvc;
-
+import com.android.myapplication.dp_world.screen.common.ObservableViewMvc;
 import java.util.List;
 
-abstract class DesignPatternListViewMvc implements ViewMvc {
+abstract class DesignPatternListViewMvc implements ObservableViewMvc<DesignPatternListViewMvc.Listener> {
     public interface Listener {
         void onDesignPatternClicked(DesignPattern designPattern);
     }
-    abstract void registerListener(Listener listener);
-    abstract void unregisterListener(Listener listener);
     abstract void bindDesignPatterns(List<DesignPattern> designPatterns);
 }
